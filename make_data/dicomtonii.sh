4905a6fc4f4084f9c1597d0f2d75f8e1bf548ec8
AravindRK99 <aravind1999@gmail.com>
2024-07-17 01:02:05 -0500

    Run dcm2niix for additional subjects

--- make_data/dicomtonii.sh
+++ make_data/dicomtonii.sh
@@ -4,6 +4,10 @@
 #     /nfs/masi/krishar1/Inhale_Exhale_CT/make_data/archive_nii.py \
 #     /nfs/masi/krishar1/Inhale_Exhale_CT/config.PRIVIA.json
 
+#  /home/local/VANDERBILT/xuk9/anaconda3/envs/python37/bin/python \
+#     /nfs/masi/krishar1/Inhale_Exhale_CT/make_data/archive_nii.py \
+#     /nfs/masi/krishar1/Inhale_Exhale_CT/config.COPDGENE.json
+
  /home/local/VANDERBILT/xuk9/anaconda3/envs/python37/bin/python \
     /nfs/masi/krishar1/Inhale_Exhale_CT/make_data/archive_nii.py \
-    /nfs/masi/krishar1/Inhale_Exhale_CT/config.COPDGENE.json
+    /nfs/masi/krishar1/Inhale_Exhale_CT/config.COPDGENE_additionalsample.json
