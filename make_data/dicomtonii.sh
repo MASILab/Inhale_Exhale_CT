7c4cfeac06b7c25993c900e3ee6313e91c399863
AravindRK99 <aravind1999@gmail.com>
2024-07-17 21:32:01 -0500

    Convert dcm to nii for controls

--- make_data/dicomtonii.sh
+++ make_data/dicomtonii.sh
@@ -8,6 +8,12 @@
 #     /nfs/masi/krishar1/Inhale_Exhale_CT/make_data/archive_nii.py \
 #     /nfs/masi/krishar1/Inhale_Exhale_CT/config.COPDGENE.json
 
+#  /home/local/VANDERBILT/xuk9/anaconda3/envs/python37/bin/python \
+#     /nfs/masi/krishar1/Inhale_Exhale_CT/make_data/archive_nii.py \
+#     /nfs/masi/krishar1/Inhale_Exhale_CT/config.COPDGENE_additionalsample.json
+
+   
+
  /home/local/VANDERBILT/xuk9/anaconda3/envs/python37/bin/python \
     /nfs/masi/krishar1/Inhale_Exhale_CT/make_data/archive_nii.py \
-    /nfs/masi/krishar1/Inhale_Exhale_CT/config.COPDGENE_additionalsample.json
+    /nfs/masi/krishar1/Inhale_Exhale_CT/config.COPDGENE_controls.json
