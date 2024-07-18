3ec7ecb9632aaf9621d722a28192ec18078b9db9
AravindRK99 <aravind1999@gmail.com>
2024-07-18 01:53:34 -0500

    Backup script

--- make_data/SPIE_data/train_test_split.py
+++ make_data/SPIE_data/train_test_split.py
@@ -3,8 +3,12 @@ import matplotlib.pyplot as plt
 import os 
 import shutil 
 
+def drop_duplicates():
+    copd_df = pd.read_csv("/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/final_paired_data_spreadsheets/COPD_GE_pairedkernels_100randomsamples_GOLDlabels.csv")
+    #Drop duplicates in the dataframe column SeriesDescription 
+    copd_df = copd_df.drop_duplicates(subset = "SeriesDescription")
+    copd_df.reset_index(drop = True, inplace = True)
+    copd_df.to_csv("/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/final_paired_data_spreadsheets/COPD_GE_pairedkernels_100randomsamples_GOLDlabels.csv", index = False)
+    print(copd_df)
 
-copd_df = pd.read_csv("/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/final_paired_data_spreadsheets/COPD_GE_pairedkernels_100randomsamples_GOLDlabels.csv")
-#Drop duplicates in the dataframe column SeriesDescription 
-copd_df = copd_df.drop_duplicates(subset = "SeriesDescription")
-print(copd_df)
+drop_duplicates()
