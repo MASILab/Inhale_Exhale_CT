bc9d3f9a23cabf13337557a02d72de392cc403b3
AravindRK99 <aravind1999@gmail.com>
2024-07-24 16:42:49 -0500

    Rigid registered images

--- voxelmorph/ANTS_register.py
+++ voxelmorph/ANTS_register.py
@@ -97,13 +97,13 @@ def mask_out_lung_test():
 
 
 def affine_register():
-    valid_bone = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/resampled/resampled_val_masked_out_BONE"
-    valid_std = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/resampled/resampled_val_masked_out_STANDARD"
+    valid_bone = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/resampled/resampled_test_masked_out_BONE"
+    valid_std = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/resampled/resampled_test_masked_out_STANDARD"
 
     # valid_bone_mask = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/lungmasks/valid/inspiratory_BONE/lung_mask"
     # valid_std_mask = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/lungmasks/valid/expiratory_STANDARD/lung_mask"
 
-    masked_out_path = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/resampled/resampled_val_masked_out_affine_registered_STANDARD"
+    masked_out_path = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/resampled/resampled_test_masked_out_rigid_registered_STANDARD"
     os.makedirs(masked_out_path, exist_ok = True)
 
     valid_bone_files = sorted(os.listdir(valid_bone))
@@ -118,11 +118,11 @@ def affine_register():
         bone = ants.image_read(os.path.join(valid_bone, valid_files[index][0]))
         std = ants.image_read(os.path.join(valid_std, valid_files[index][1]))
 
-        affine_reg = ants.registration(fixed = bone, moving = std, type_of_transform = 'Affine')
+        affine_reg = ants.registration(fixed = bone, moving = std, type_of_transform = 'Rigid')
 
         warped_image = ants.apply_transforms(fixed = bone, moving = std, transformlist = affine_reg['fwdtransforms'])
 
-        ants.image_write(warped_image, os.path.join(masked_out_path, f"affine_registered_{valid_files[index][1]}"))
+        ants.image_write(warped_image, os.path.join(masked_out_path, f"rigid_registered_{valid_files[index][1]}"))
 
 
 affine_register()
