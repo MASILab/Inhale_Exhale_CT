14bd11895e8d870396e3b9f503609b80d6b0fb13
AravindRK99 <aravind1999@gmail.com>
2024-07-24 22:56:57 -0500

    Changed directory structure

---
+++ voxmorphdev/dataloader.py
@@ -0,0 +1,53 @@
+import nibabel as nib 
+import numpy as np
+import os
+os.environ['NEURITE_BACKEND'] = 'pytorch'
+os.environ['VXM_BACKEND'] = 'pytorch' 
+import torch
+from torch.utils.data import Dataset, DataLoader
+import pandas as pd 
+from scipy.interpolate import interp1d
+
+
+
+class VoxelMorphDataloader(Dataset):
+    def __init__(self, dataframe):
+        self.dataframe = dataframe
+        self.normalizer = interp1d([-1024, 0], [0, 1])
+
+    def __len__(self):
+        return len(self.dataframe)
+    
+    def __getitem__(self, idx):
+        row = self.dataframe.loc[idx]
+        pid = row['FileName']
+        insp_bone = row['inspiratory_BONE']
+        exp_std = row['expiratory_STANDARD']
+
+        bone_tensor = self.normalize(insp_bone)
+        std_tensor = self.normalize(exp_std)
+
+        return [std_tensor, bone_tensor], [bone_tensor] #Return moving and fixed image along with the fixe image gorund tensor 
+
+
+    def normalize(self, data):
+        img = nib.load(data).get_fdata()
+        clipped_img = np.clip(img, -1024, 0)
+        norm_img = self.normalizer(clipped_img)
+        torch_tensor = torch.from_numpy(norm_img).float()
+        return torch_tensor
+ 
+
+ #Test dataloader 
+train_df_harm = pd.read_csv("/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/harmonized/test_data_voxelmorph.csv")
+val_df_val = pd.read_csv("/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/harmonizedvalid_data_voxelmorph.csv")
+
+train_dataset = VoxelMorphDataloader(train_df_harm)
+valid_dataset = VoxelMorphDataloader(val_df_val)
+
+train_loader = DataLoader(dataset=train_dataset, batch_size=4, shuffle=True, num_workers=6)
+valid_loader = DataLoader(dataset=valid_dataset, batch_size=4, shuffle=False, num_workers=6)
+
+for i, (data, target) in enumerate(train_loader):
+    print(data[0].shape, data[1].shape, target[0].shape)
+    break
