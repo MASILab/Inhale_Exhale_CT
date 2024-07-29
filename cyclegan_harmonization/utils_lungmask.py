243dafaf4045c68ce96afc3ec35b97499c347615
AravindRK99 <aravind1999@gmail.com>
2024-07-28 23:52:03 -0500

    Lung mask for registered image

--- cyclegan_harmonization/utils_lungmask.py
+++ cyclegan_harmonization/utils_lungmask.py
@@ -45,9 +45,22 @@ paths = ["/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/train/inspirato
 val_paths = ["/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/valid/inspiratory_BONE", "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/valid/expiratory_STANDARD"]
 test_paths = ["/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/test/inspiratory_BONE", "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/test/expiratory_STANDARD"]
 
-for file_path in test_paths:
+harmonized_data = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/experiments/insp_exp_run1_results/insp_exp_COPD_epoch5_run1_outputs"
+out_path = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/lungmasks/harmonized_epoch5"
+
+harmonized_val_data = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/experiments/insp_exp_run1_results_cycleGAN/insp_exp_COPD_epoch5_val"
+harmonized_val_mask_out = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/lungmasks/harmonized_epoch5_val"
+
+# for file_path in harmonized_val_data:
+reg_paths = ["/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/harmonized/SyN_registered_STANDARD_expiratory_images", 
+             "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/harmonized/SyN_registered_BONE_inspiratory_images_without_mask", 
+             "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/SyN_registered_BONE_inspiratory_images_without_mask", 
+             "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_data/non_harmonized/SyN_registered_STANDARD_expiratory_images"]
+
+
+for file_path in tqdm(reg_paths):
     intctdir = file_path
-    outct_dir = file_path.replace('test', 'lungmasks/test')
+    outct_dir = os.path.join(file_path + "_lungmask")
     os.makedirs(outct_dir, exist_ok=True)
     lungmask = GetLungMask(intctdir, outct_dir)
     lungmask.generate_lung_mask()
