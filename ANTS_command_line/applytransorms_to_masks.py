226936748b0119d83389e414df61739efc4ed648
AravindRK99 <aravind1999@gmail.com>
2024-08-01 11:00:28 -0500

    Ran nearest neighbor interpolation for the masks

---
+++ ANTS_command_line/applytransorms_to_masks.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+from tqdm import tqdm
+
+
+#apply deformation fields to the lung masks to get the deformed masks 
+# fixed_inhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/masks/inference_lung_masks/inspiratory_BONE"
+fixed_inhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/masks/harmonized_BONE_lung_masks"
+moving_exhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/masks/inference_lung_masks/expiratory_STANDARD"
+
+# non_harmonized_transforms = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exp_toinsp_nonharmonized"
+
+harmonized_transforms = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exptoinsp_harmonized"
+
+# output_lung_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exp_toinsp_nonharmonized_emphysema/lung_masks" 
+output_lung_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exptoinsp_harmonized_emphysema/lung_masks"
+
+fixed_masks = sorted(os.listdir(fixed_inhalation_masks))
+moving_masks = sorted(os.listdir(moving_exhalation_masks))
+transforms = sorted(os.listdir(harmonized_transforms))
+
+for fixed, moving, transform in tqdm(zip(fixed_masks, moving_masks, transforms), total=len(fixed_masks)):
+    fixed_mask_path = os.path.join(fixed_inhalation_masks, fixed)
+    moving_mask_path = os.path.join(moving_exhalation_masks, moving)
+    transform_path = os.path.join(harmonized_transforms, transform)
+
+    files_transforms = os.listdir(transform_path)
+    for file in files_transforms:
+        if file.endswith("_1Warp.nii.gz"):
+            warp_field = os.path.join(harmonized_transforms, transform, file)
+        if file.endswith("_0GenericAffine.mat"):
+            affine_field = os.path.join(harmonized_transforms, transform, file)
+        if file.endswith("_Warped.nii.gz"):
+            outfile = file
+
+    output_mask_path = os.path.join(output_lung_masks, outfile)
+
+    print("Deforming the mask for ", moving)
+    print("Apply the ANTS transforms")
+
+    #Apply the transform to the moving mask
+    command = f"antsApplyTransforms -d 3 -i {moving_mask_path} -r {fixed_mask_path} -t {warp_field} -t {affine_field} -n NearestNeighbor -o {output_mask_path}"
+    print(command)
+    os.system(command)
