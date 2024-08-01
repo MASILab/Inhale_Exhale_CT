1d576c8817e92f420c25e666ad792660aa129db2
AravindRK99 <aravind1999@gmail.com>
2024-08-01 11:34:15 -0500

    Log jacobian

---
+++ ANTS_command_line/logjacobdet.py
@@ -0,0 +1,20 @@
+import os
+import numpy as np
+from tqdm import tqdm
+
+
+non_harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exp_toinsp_nonharmonized" 
+harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exptoinsp_harmonized"
+
+#Compute log jacobian determinant using ANTS 
+
+for file in tqdm(os.listdir(harmonized)):
+    files = os.listdir(os.path.join(harmonized, file))
+    for f in files:
+        if f.endswith("_1Warp.nii.gz"):
+            warp_field = os.path.join(harmonized, file, f)
+    
+    output = os.path.join(harmonized, file, "log_jacobian_det.nii.gz")
+    command = f"CreateJacobianDeterminantImage 3 {warp_field} {output} 1 1"
+    print(command)
+    os.system(command)
