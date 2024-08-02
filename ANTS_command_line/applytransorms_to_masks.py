438be5dafa2897787c1e7ca9dba220a3d96ed5b1
AravindRK99 <aravind1999@gmail.com>
2024-08-02 14:28:37 -0500

    Inverse transforms

--- ANTS_command_line/applytransorms_to_masks.py
+++ ANTS_command_line/applytransorms_to_masks.py
@@ -12,8 +12,8 @@ moving_exhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_spli
 
 harmonized_transforms = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exptoinsp_harmonized"
 
-# output_lung_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exp_toinsp_nonharmonized_emphysema/warped_emphysema_masks" 
-output_lung_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exptoinsp_harmonized_emphysema/warped_emphysema_masks"
+# output_lung_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exp_toinsp_nonharmonized_emphysema/warped_emphysema_insp_to_exp" 
+output_lung_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exptoinsp_harmonized_emphysema/warped_emphysema_insp_to_exp"
 
 fixed_masks = sorted(os.listdir(fixed_inhalation_masks))
 moving_masks = sorted(os.listdir(moving_exhalation_masks))
@@ -26,11 +26,11 @@ for fixed, moving, transform in tqdm(zip(fixed_masks, moving_masks, transforms),
 
     files_transforms = os.listdir(transform_path)
     for file in files_transforms:
-        if file.endswith("_1Warp.nii.gz"):
+        if file.endswith("_1InverseWarp.nii.gz"):
             warp_field = os.path.join(harmonized_transforms, transform, file)
         if file.endswith("_0GenericAffine.mat"):
             affine_field = os.path.join(harmonized_transforms, transform, file)
-        if file.endswith("_Warped.nii.gz"):
+        if file.endswith("_InverseWarped.nii.gz"):
             outfile = file
 
     output_mask_path = os.path.join(output_lung_masks, outfile)
@@ -39,6 +39,7 @@ for fixed, moving, transform in tqdm(zip(fixed_masks, moving_masks, transforms),
     print("Apply the ANTS transforms")
 
     #Apply the transform to the moving mask
-    command = f"antsApplyTransforms -d 3 -i {moving_mask_path} -r {fixed_mask_path} -t {warp_field} -t {affine_field} -n NearestNeighbor -o {output_mask_path}"
+    # command = f"antsApplyTransforms -d 3 -i {moving_mask_path} -r {fixed_mask_path} -t {warp_field} -t {affine_field} -n NearestNeighbor -o {output_mask_path}"
+    command = f"antsApplyTransforms -d 3 -i {fixed_mask_path} -r {moving_mask_path} -t {warp_field} -t {[ affine_field, 1 ]} -n NearestNeighbor -o {output_mask_path}"
     print(command)
     os.system(command)
