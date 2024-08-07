e9ed8ae80b7b778505cf56a9f05da1c90298a79a
AravindRK99 <aravind1999@gmail.com>
2024-08-07 14:05:18 -0500

    Warped emphysema masks and log jacobian determinants

--- ANTS_command_line/applytransforms_to_masks.py
+++ ANTS_command_line/applytransforms_to_masks.py
@@ -86,4 +86,88 @@ def run_emphysema_warp_rigid_reg():
         os.system(command)
 
 # run_emphysema_warp_rigid_reg()
-run_emphysema_warp_SyN()
+# run_emphysema_warp_SyN()
+
+def run_emphysema_warp_ANTS_full_SyN():
+    #Do for harmonized then non harmonized
+    fixed_inhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/experiments/insp_exp_run1_results_cycleGAN/harmonized_emphysema_epoch5/emphysema"
+    moving_exhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/val_test/exp_STANDARD_emphysema/emphysema"
+    harmonized_transforms = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/harm_ANTS_outputs_exptoinsp_large_images" 
+
+    output_warp_harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/harm_ANTS_outputs_exptoinsp_large_images_emphysema_warped"
+    os.makedirs(output_warp_harmonized, exist_ok=True)
+
+    required_files = [file for file in os.listdir(harmonized_transforms)]
+
+    #remove the masked word at the beginning of the files in the list comprehesion and replace STANDARD with BONE and also add a .nii.gz at the end
+    required_files_fixed = sorted([file.replace("masked_", "").replace("STANDARD", "BONE") + ".nii.gz" for file in required_files])
+    required_files_moving = sorted([file.replace("masked_", "") + ".nii.gz" for file in required_files])
+    transforms = sorted(os.listdir(harmonized_transforms))
+
+    for fixed, moving, transform in tqdm(zip(required_files_fixed, required_files_moving, transforms), total=len(required_files_fixed)):
+        fixed_mask_path = os.path.join(fixed_inhalation_masks, fixed)
+        moving_mask_path = os.path.join(moving_exhalation_masks, moving)
+        transform_path = os.path.join(harmonized_transforms, transform)
+
+        files_transforms = os.listdir(transform_path)
+        for file in files_transforms:
+            if file.endswith("_1Warp.nii.gz"):
+                warp_field = os.path.join(harmonized_transforms, transform, file)
+            if file.endswith("_0GenericAffine.mat"):
+                affine_field = os.path.join(harmonized_transforms, transform, file)
+            if file.endswith("_Warped.nii.gz"):
+                outfile = file
+
+        output_mask_path = os.path.join(output_warp_harmonized, outfile)
+
+        print("Deforming the mask for ", moving)
+        print("Apply the ANTS transforms")
+
+        #Apply the transform to the moving mask
+        command = f"antsApplyTransforms -d 3 -i {moving_mask_path} -r {fixed_mask_path} -t {warp_field} -t {affine_field} -n NearestNeighbor -o {output_mask_path}"
+        print(command)
+        os.system(command)
+
+
+def run_emphysema_warp_ANTS_full_SyN_nonharmonized():
+    #Do for harmonized then non harmonized
+    fixed_inhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/val_test/insp_BONE_emphysema/emphysema"
+    moving_exhalation_masks = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/val_test/exp_STANDARD_emphysema/emphysema"
+    nonharmonized_transforms = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/nonharm_ANTS_outputs_exptoinsp_large_images" 
+
+    output_warp_harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/nonharm_ANTS_outputs_exptoinsp_large_images_emphysema_warped"
+    os.makedirs(output_warp_harmonized, exist_ok=True)
+
+    required_files = [file for file in os.listdir(nonharmonized_transforms)]
+
+    #remove the masked word at the beginning of the files in the list comprehesion and replace STANDARD with BONE and also add a .nii.gz at the end
+    required_files_fixed = sorted([file.replace("masked_", "").replace("STANDARD", "BONE") + ".nii.gz" for file in required_files])
+    required_files_moving = sorted([file.replace("masked_", "") + ".nii.gz" for file in required_files])
+    transforms = sorted(os.listdir(nonharmonized_transforms))
+
+    for fixed, moving, transform in tqdm(zip(required_files_fixed, required_files_moving, transforms), total=len(required_files_fixed)):
+        fixed_mask_path = os.path.join(fixed_inhalation_masks, fixed)
+        moving_mask_path = os.path.join(moving_exhalation_masks, moving)
+        transform_path = os.path.join(nonharmonized_transforms, transform)
+
+        files_transforms = os.listdir(transform_path)
+        for file in files_transforms:
+            if file.endswith("_1Warp.nii.gz"):
+                warp_field = os.path.join(nonharmonized_transforms, transform, file)
+            if file.endswith("_0GenericAffine.mat"):
+                affine_field = os.path.join(nonharmonized_transforms, transform, file)
+            if file.endswith("_Warped.nii.gz"):
+                outfile = file
+
+        output_mask_path = os.path.join(output_warp_harmonized, outfile)
+
+        print("Deforming the mask for ", moving)
+        print("Apply the ANTS transforms")
+
+        #Apply the transform to the moving mask
+        command = f"antsApplyTransforms -d 3 -i {moving_mask_path} -r {fixed_mask_path} -t {warp_field} -t {affine_field} -n NearestNeighbor -o {output_mask_path}"
+        print(command)
+        os.system(command)
+
+# run_emphysema_warp_ANTS_full_SyN()
+run_emphysema_warp_ANTS_full_SyN_nonharmonized()
--- ANTS_command_line/plotjacobdet.py
+++ ANTS_command_line/plotjacobdet.py
@@ -3,18 +3,18 @@ import numpy as np
 from tqdm import tqdm
 
 
-non_harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exp_toinsp_nonharmonized" 
-harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/ANTS_outputs_exptoinsp_harmonized"
+non_harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/nonharm_ANTS_outputs_exptoinsp_large_images" 
+harmonized = "/nfs/masi/krishar1/SPIE_2025_InhaleExhaleCT/data_split/registration_ANTS_command_line/harm_ANTS_outputs_exptoinsp_large_images"
 
 #Compute log jacobian determinant using ANTS 
 
-for file in tqdm(os.listdir(non_harmonized)):
-    files = os.listdir(os.path.join(non_harmonized, file))
+for file in tqdm(os.listdir(harmonized)):
+    files = os.listdir(os.path.join(harmonized, file))
     for f in files:
         if f.endswith("_1Warp.nii.gz"):
-            warp_field = os.path.join(non_harmonized, file, f)
+            warp_field = os.path.join(harmonized, file, f)
     
-    output = os.path.join(non_harmonized, file, "jacobian_det.nii.gz")
-    command = f"CreateJacobianDeterminantImage 3 {warp_field} {output} 0 1"
+    output = os.path.join(harmonized, file, "log_jacobian_det.nii.gz")
+    command = f"CreateJacobianDeterminantImage 3 {warp_field} {output} 1 1"
     print(command)
     os.system(command)
